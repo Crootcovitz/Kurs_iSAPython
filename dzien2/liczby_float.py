c6f4a904570c0d3fc59bf9a63c30c3d148df0153
Crootcovitz <33104621+Crootcovitz@users.noreply.github.com>
2017-11-07 20:33:09 +0100

    pliki dzien 2

---
+++ dzien2/liczby_float.py
@@ -0,0 +1,13 @@
+a=0.1+0.1+0.1
+b=0.3
+wynik = a==b
+
+print(wynik)
+print(f'a = {a}, b = {b}')
+
+liczba_a=round(45.454545)
+print(liczba_a)
+print(round(45.454545))
+
+wynik = round(a, 3) == round(b, 3)
+print(wynik)
---
+++ dzien2/zmienne.py
@@ -0,0 +1,13 @@
+imie='Joanna'
+nazwisko='Kowalska'
+wiek=34
+# print(imie)
+# print(imie[3:5])
+# print(3+7)
+# print(3==10)
+# print(imie.lower())
+# print(imie.count('a'))
+print(imie+' '+nazwisko+' ma '+str(wiek)+' lata.')
+print(f"{imie} {nazwisko} ma {wiek} lata.")
+print('{0} {1} ma {2} lata.'.format(imie, nazwisko, wiek))
+print('%s %s ma %d lata.' % (imie, nazwisko, wiek))
