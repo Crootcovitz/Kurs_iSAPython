fecb9e7ab1821f603d65d4197ab8aead68de88de
Crootcovitz <33104621+Crootcovitz@users.noreply.github.com>
2017-11-13 01:39:23 +0100

    pliki dzien 3

--- dzien3/praca_domowa_temperatury.py
+++ dzien3/praca_domowa_temperatury.py
@@ -6,8 +6,8 @@ typ = temperatura[-1]
 if typ == 'C':
     nowa_wartosc = wartosc * (9 / 5) + 32
     nowy_typ = 'F'
-    print(f'Podana temperatura w {nowy_typ} to {nowa_wartosc} stopni.')
 elif typ == 'F':
     nowa_wartosc = (wartosc - 32) * (5/9)
     nowy_typ = 'C'
-    print(f'Podana temperatura w {nowy_typ} to {nowa_wartosc:.{4}} stopni.')
+
+print(f'Podana temperatura w {nowy_typ} to {nowa_wartosc:.{4}} stopni.')
